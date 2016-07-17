9a2c5cc8ab06c4e100b4f4ddf0f321394c6adf70
root <root@localhost.localdomain>
2016-07-17 08:50:09 +0200

    minmaxfix2

--- othello.js
+++ othello.js
@@ -151,17 +151,37 @@ Othello.prototype.Exit = function()
 
 Othello.prototype.evalute = function(board, color)
 {
-  var me = this.Countcolor(board, color);
+  //var me = this.Countcolor(board, color);
   var enemy = color == this.white ? this.black : this.white;
-  if(board[0][0] == color) me += 10;
-  else if(board[0][0] == enemy) me -= 10;
+ /* if(board[0][0] == color) me += 10;
+  else if(board[0][0] == enemy) me -= 5;
   if(board[0][7] == color) me += 10;
-  else if(board[0][7] == enemy) me -= 10;
+  else if(board[0][7] == enemy) me -= 5;
   if(board[7][0] == color) me += 10;
-  else if(board[7][0] == enemy) me -= 10;
+  else if(board[7][0] == enemy) me -= 5;
   if(board[7][7] == color) me += 10;
-  else if(board[7][7] == enemy) me -= 10;
-  return(me);
+  else if(board[7][7] == enemy) me -= 5;
+  return(me);*/
+
+     var evaluateTable = [
+      [ 30, -5,  0, -1, -1,  0, -5,  30],
+      [-5, -15, -3, -3, -3, -3, -15, -5],
+      [  0,  -3,  0, -1, -1,  0,  -3,   0],
+      [ -1,  -3, -1, -1, -1, -1,  -3,  -1],
+      [ -1,  -3, -1, -1, -1, -1,  -3,  -1],
+      [  0,  -3,  0, -1, -1,  0,  -3,   0],
+      [-5, -15, -3, -3, -3, -3, -15, -5],
+      [ 30, -5,  0, -1, -1,  0, -5,  30]
+    ];
+    var value = 0;
+
+    for(var i = 0; i < 8; i++) {
+      for(var j = 0; j < 8; j++) {
+        if(color == this.board[i][j]) value += evaluateTable[i][j];
+        else if(enemy == this.board[i][j]) value -= evaluateTable[i][j];
+      }
+    }
+    return(value);
 };
 
 Othello.prototype.negamaxSearch = function(color, depth)
